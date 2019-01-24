4f90f7f0a1e64082fa98338d581136fac9b2aa5c
Manish Sharma <mansha99@gmail.com>
2019-01-24 22:33:22 +0530

    Updated-02

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,12 +1,29 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     //$("#profile-container").load("includes/profile-container.html");
-    ms_load("#profile-container","includes/profile-container.html");
+    var elements = ["profile-container",
+        "contact-container",
+        "education-container",
+        "languages-container",
+        "interests-container",
+        "summary-section",
+        "showcase-section",
+        "experiences-section",
+        "projects-section",
+        "skills-section"
+
+    ];
+    for (i = 0; i < elements.length; i++) {
+        var el = elements[i];
+        ms_load("#" + el, "includes/" + el + ".html");
+    }
+
+
 });
-function ms_load(ele,url){
+/********************************************************************************/
+function ms_load(ele, url) {
     $.ajax({
-        url:url,
+        url: url,
         success: function (data, textStatus, jqXHR) {
-            //alert(data);
             $(ele).html(data);
         }
     });
