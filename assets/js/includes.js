5af911d022e07d17d61ea8073f7ae38debf3262f
Manish Sharma <mansha99@gmail.com>
2019-01-25 20:41:42 +0530

    update-06

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,30 +1,37 @@
 $(document).ready(function () {
     //$("#profile-container").load("includes/profile-container.html");
-    var elements = ["profile-container",
-        "contact-container",
-        "education-container",
-        "languages-container",
-        "interests-container",
-        "summary-section",
-        "showcase-section",
-        "experiences-section",
-        "projects-section",
-        "skills-section"
+    var elements = [
+        {"k": "profile-container", "f": function_empty},
+        {"k": "contact-container", "f": function_empty},
+        {"k": "education-container", "f": function_empty},
+        {"k": "languages-container", "f": function_empty},
+        {"k": "interests-container", "f": function_empty},
+        {"k": "summary-section", "f": function_empty},
+        {"k": "showcase-section", "f": function_empty},
+        {"k": "experiences-section", "f": function_empty},
+        {"k": "projects-section", "f": function_empty},
+        {"k": "skills-section", "f": function_empty}
 
     ];
     for (i = 0; i < elements.length; i++) {
         var el = elements[i];
-        ms_load("#" + el, "includes/" + el + ".html");
+        ms_load("#" + el.k, "includes/" + el.k + ".html", el.f);
     }
 
 
 });
 /********************************************************************************/
-function ms_load(ele, url) {
+function function_empty() {
+    console.log("empty" + new Date().toDateString());
+}
+/********************************************************************************/
+function ms_load(ele, url, fun) {
     $.ajax({
         url: url,
         success: function (data, textStatus, jqXHR) {
             $(ele).html(data);
+            fun();
+
         }
     });
 }
